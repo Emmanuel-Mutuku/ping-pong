e609e8e8accf260b24d00d7e2e9571c2377b653b
Emmanuel Mutuku <manueldizzy@gmail.com>
2018-02-26 11:21:39 +0300

    Add ping

--- js/scripts.js
+++ js/scripts.js
@@ -18,29 +18,24 @@
 //         $("#result").show();
 //       });
 //     });
-function pingpong(number) {
-    var myNumber = [];
-    for (var j = 1; j <= number; j++) {
-        if (j % 15 === 0) {
-            myNumber.push("PING PONG");
-        } else if (j % 5 === 0) {
-            myNumber.push("PONG");
-        } else if (j % 3 === 0) {
-            myNumber.push("PING");
-        } else {
-            myNumber.push(j);
-        }
-    };
-    return myNumber;
-};
-
 $(document).ready(function() {
-    $("form#Ping-Pong").submit(function(event) {
+    $("form#ping-pong").submit(function(event) {
         event.preventDefault();
-        $("li.res").empty();
-        var j = parseInt($("input#Ping-Pong").val());
-        var derivedNumber = pingpong(j) + '';
-        var splitNumber = derivedNumber.split(",").join("<br>");
-        $("div.result").addClass('result').append("<li class='res'>" + splitNumber + "</li>");
+        display = parseInt($("input#number").val());
+        var output = count(display);
+
     });
 });
+
+// business logic
+var display;
+var count = function (){
+  for (var i = 1; i <= display; i++) {
+        // if the number is divisible by 3, write "Ping"
+        //  if ( i % 3 === 0) {
+        //   $(".Push").append("<li>"+"ping"+"</>");
+        //   return "Ping";
+        // }
+    $("#result").append("<li>"+i+"</li>");
+  }
+}
